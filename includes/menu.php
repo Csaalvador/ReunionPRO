814195dd81ade8087d2e0a6f7abc94d90151fe4f
Csaalvador <caua.salvador.dev@gmail.com>
2024-07-26 19:42:12 -0300

    fix: apareça ReunionPro

--- includes/menu.php
+++ includes/menu.php
@@ -5,7 +5,7 @@ $current_page = basename($_SERVER['PHP_SELF']);
 <div id="sidebar" class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px;">
     <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
         <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
-        <span class="fs-4">Gerenciamento</span>
+        <span class="fs-4">ReunionPro</span>
     </a>
     <hr>
     <ul class="nav nav-pills flex-column mb-auto">
