5471039c6fa3898508c2b8c14fc6735de2d1aa1b
Csaalvador <caua.salvador.dev@gmail.com>
2024-07-27 19:51:17 -0300

    fix - ajustando ícones do sistema

--- includes/messages.php
+++ includes/messages.php
@@ -61,8 +61,8 @@ function display_messages() {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <style>
         .popup-img {
-            width: 50px;
-            height: 50px;
+            width: 16px;
+            height: 16px;
         }
     </style>
 </head>
