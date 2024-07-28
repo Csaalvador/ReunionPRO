69fb1aa16208fe20e53612840209bca9a90893f6
Csaalvador <caua.salvador.dev@gmail.com>
2024-07-27 22:24:06 -0300

    fix style - login

--- login.php
+++ login.php
@@ -7,25 +7,29 @@ require 'includes/messages.php';
 <html lang="pt-br" data-bs-theme="dark">
 <head>
     <title>Acesso</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" ;
-"
->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="icon" type="image/x-icon" href="images/favicon16.png">
+
+    <style>
+                .form-label {
+            background-color: transparent !important;
+        }
+    </style>
 </head>
 <body>
     <div class="d-flex vh-100">
         <div class="container d-flex justify-content-center align-items-center">
             <div class="text-center w-50">
                 <figure class="figure">
-                    <img src="images/login.png" class="figure-img img-fluid rounded login" alt="capivara" style="width: 145px">
+                    <img src="images/login.png" class="figure-img img-fluid rounded login" alt="reuniao" style="width: 130px;">
                 </figure>
                 <h2 class="mb-4">Login</h2>
                 <form class="form-signin login-form w-100 m-auto" method="post" action="includes/check_login.php">
                     <?php include 'includes/toasts.php'; ?>
                     <div class="form-floating form-email">
                         <input type="email" class="form-control" id="email" name="email" placeholder="Email" required>
-                        <label for="email" class="form-label fw-normal">Endereço de E-mail</label>
+                        <label for="email" class="form-label fw-normal" style=>Endereço de E-mail</label>
                     </div>
                     <div class="form-floating form-senha">
                         <input type="password" class="form-control" id="password" name="password" placeholder="Senha" required>
@@ -41,3 +45,7 @@ require 'includes/messages.php';
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
+<?php
+
+?>
